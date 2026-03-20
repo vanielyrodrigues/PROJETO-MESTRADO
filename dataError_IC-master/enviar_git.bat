b118e4e4c88107e55fc9ca914f9264997b2c169f
Vaniely Rodrigues <vanielyrodrigues@gmail.com>
2026-03-19 21:48:18 -0300

    Atualização para resolver a classificação - 2026-03-19 21-48

--- dataError_IC-master/enviar_git.bat
+++ dataError_IC-master/enviar_git.bat
@@ -4,9 +4,20 @@ cd /d C:\PROJETO\dataError_IC-master
 echo.
 set /p msg=Digite a mensagem do commit: 
 
+:: Captura data no formato YYYY-MM-DD
+for /f "tokens=1-3 delims=/" %%a in ("%date%") do set DATA=%%c-%%b-%%a
+
+:: Captura hora no formato HH-MM
+for /f "tokens=1-2 delims=:" %%a in ("%time%") do set HORA=%%a-%%b
+
+echo.
+echo Enviando com mensagem:
+echo %msg% - %DATA% %HORA%
+echo.
+
 git status
 git add .
-git commit -m "%msg%"
+git commit -m "%msg% - %DATA% %HORA%"
 git push origin main
 
 echo.
